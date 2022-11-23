11212d23bb8f2c4b88112297983eafdd3e84fe8a
Peter Fishman <98246441+PeterFishmann@users.noreply.github.com>
2022-11-23 09:39:43 -0500

    Login signup activity

--- AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/MainActivity.java
+++ AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/MainActivity.java
@@ -4,25 +4,42 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open,
-                R.string.nav_close);
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
 
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        drawerLayout = findViewById(R.id.drawer_layout);
+//        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open,
+//                R.string.nav_close);
+//
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 }
--- AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/RegisterActivity.java
+++ AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.appdev2finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView alreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        alreadyHaveAccount = findViewById(R.id.alreadyHaveAccount);
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
