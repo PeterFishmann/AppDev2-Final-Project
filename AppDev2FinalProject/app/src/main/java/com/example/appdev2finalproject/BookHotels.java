548af0004f8e28b21d1f7bc3cb3becfc4749011e
Alihan Djamankulov <dubashevalihan@gmail.com>
2022-12-01 15:23:51 -0500

    Revert "Merge branch 'main' into old-state"
    This reverts commit 1cc069aa4cd1d60fd35ba701ca412ee47c82bfad, reversing
    changes made to 41e5fd20bb07c540e21332937f749eb08826adba.

---
+++ AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/BookHotels.java
@@ -0,0 +1,23 @@
+package com.example.appdev2finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class BookHotels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_hotels);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
--- AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/MainActivity.java
+++ AppDev2FinalProject/app/src/main/java/com/example/appdev2finalproject/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Dashboard.class));
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
 
